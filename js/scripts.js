0908a687cf23c6deaba3a4d54baa6bb3a7efb37d
aaba00 <ab9761@nyu.edu>
2023-04-20 15:39:09 -0400

    basic point map of homeless reports

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,28 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWFiYTAwIiwiYSI6ImNsZzVxaWltcTA1dnczaHFyc3NrZXc4N20ifQ.HHHdXxGVb4zlQcNg1CwEZg';
 
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: 'mapbox://styles/mapbox/dark-v11', // style URL
-    center: [-73.99330880814004, 40.73762668306899], // starting position [lng, lat]
-    zoom: 10 // starting zoom
-    });   
+  container: 'map', // container ID
+  // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+  style: 'mapbox://styles/mapbox/dark-v11', // style URL
+  center: [-73.99330880814004, 40.73762668306899], // starting position [lng, lat]
+  zoom: 10 // starting zoom
+});
 
-map.on('load', function(){
-  //map.addSource('my-points). {
-    // type: 'geojson',
-    // data: myPoints
-})
+map.on('load', () => {
+  map.addSource('homeless_reports', {
+    type: 'geojson',
+    data: 'data/homeless_reports_points.geojson'
+  }),
 
-map.addLayer({ //every layer must have one source, but one source can power many layers
-  //id: '[data type]-[source]'
-  //type: circle
-  //data: myPoints
-
-})
+    map.addLayer({
+      'id': 'reports-layer',
+      'type': 'circle',
+      'source': 'homeless_reports',
+      'paint': {
+        'circle-radius': 4,
+        'circle-stroke-width': 2,
+        'circle-color': 'red',
+        'circle-stroke-color': 'white'
+      }
+    });
+});
